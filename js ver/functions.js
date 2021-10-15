94dcbfd2bb3b19e451aba4b96c3b726e386aea91
mllorach <martillorachnaharro@gmail.com>
2021-10-15 13:01:03 +0200

    actualización funcion recursiva, falta testeo

--- js ver/functions.js
+++ js ver/functions.js
@@ -13,15 +13,138 @@ function hasDuplicates(arr) {
     return new Set(arr).size !== arr.length;
 }
 
-function isSolvable(sudoku, rowNum, colNum){
-
-    
-
+function countInArray(array, what) {
+    var count = 0;
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] === what) {
+            count++;
+        }
+    }
+    return count;
 }
-function solveSudoku(){
 
+function isSolvable(sudoku, rowNum, colNum){
+    let row = sudoku[rowNum];
+    let col = [];
+    for (let index = 0; index < 9; index++) {
+        col.push(sudoku[index][colNum]);
+    }
+    let block = [];
+    let bX = Math.floor(colNum / 3);
+    let bY = Math.floor(rowNum / 3);
+    let startX = bX * 3;
+    let startY = bY * 3;
+    for (let blockY = startY; blockY < startY+3; blockY++){
+        for (let blockX = startX; blockX < startX+3; blockX++){
+            block.push(sudoku[blockY][blockX]);
+        }
+    }
 
-    
+    if (sudoku[rowNum][colNum]!=''){
+        let num = sudoku[rowNum][colNum] + '';
+        if (countInArray(row, num)==1 && countInArray(row, num)==1 && countInArray(row, num)==1){
+            if (rowNum==8 && colNum==8){
+                return true;
+            } else if (colNum>7){
+                if (isSolvable(sudoku, (rowNum+1), 0)) return true;
+            } else {
+                if (isSolvable(sudoku, rowNum, (colNum+1))) return true;
+            }
+        }
+        return false;
+    }
+
+    for (let i = 1; i <= 9; i++) {
+        i = i + '';
+        if (!inArray(row, i) && !inArray(col, i) && !inArray(block, i)){
+            sudoku[rowNum][colNum] = i;
+            if (rowNum==8 && colNum==8){
+                return true;
+            }
+            if (colNum>7){
+                if (isSolvable(sudoku, (rowNum+1), 0)){
+                    return true;
+                }
+            } else {
+                if (isSolvable(sudoku, rowNum, (colNum+1))){
+                    return true;
+                }
+            }
+            sudoku[rowNum][colNum] = '';
+        }
+    }
+    return false;    
+}
+
+function solveSudoku(sudoku, rowNum, colNum){
+    let row = sudoku[rowNum];
+    let col = [];
+    for (let index = 0; index < 9; index++) {
+        col.push(sudoku[index][colNum]);
+    }
+    let block = [];
+    let bX = Math.floor(colNum / 3);
+    let bY = Math.floor(rowNum / 3);
+    let startX = bX * 3;
+    let startY = bY * 3;
+    for (let blockY = startY; blockY < startY+3; blockY++){
+        for (let blockX = startX; blockX < startX+3; blockX++){
+            block.push(sudoku[blockY][blockX]);
+        }
+    }
+
+    if (sudoku[rowNum][colNum]!=''){
+        let num = sudoku[rowNum][colNum] + '';
+        if (countInArray(row, num)==1 && countInArray(row, num)==1 && countInArray(row, num)==1){
+            if (rowNum==8 && colNum==8){
+                printSudoku(sudoku)
+                return true;
+            } else if (colNum>7){
+                if (solveSudoku(sudoku, (rowNum+1), 0)) return true;
+            } else {
+                if (solveSudoku(sudoku, rowNum, (colNum+1))) return true;
+            }
+        }
+        return false;
+    }
+
+    for (let i = 1; i <= 9; i++) {
+        i = i + '';
+        if (!inArray(row, i) && !inArray(col, i) && !inArray(block, i)){
+            sudoku[rowNum][colNum] = i;
+            if (rowNum==8 && colNum==8){
+                printSudoku(sudoku)
+                return true;
+            }
+            if (colNum>7){
+                if (solveSudoku(sudoku, (rowNum+1), 0)){
+                    return true;
+                }
+            } else {
+                if (solveSudoku(sudoku, rowNum, (colNum+1))){
+                    return true;
+                }
+            }
+            sudoku[rowNum][colNum] = '';
+        }
+    }
+    return false;       
+}
+
+function trySolveSudoku(){
+    if (!solveSudoku(getSudoku(),0,0)){
+        document.getElementsByTagName('p')[0].innerHTML = "NO TE SOLUCIÓ";
+    } else{
+        document.getElementsByTagName('p')[0].innerHTML = "SOLUCIÓ";
+    }
+}
+
+function tryIsSolvable(){
+    if (isSolvable(getSudoku(),0,0)){
+        document.getElementsByTagName('p')[0].innerHTML = "TE SOLUCIÓ";
+    } else{
+        document.getElementsByTagName('p')[0].innerHTML = "NO TE SOLUCIÓ";
+    }
 }
 
 function isSolved(sudoku){
@@ -35,17 +158,13 @@ function isSolved(sudoku){
         }
        
         let block = [];
-        let bX = Math.floor(randX / 3);
-        let bY = Math.floor(randY / 3);
-        let startX = bX * 3;
-        let startY = bY * 3;
-
+        let startX = Math.floor(y / 3) * 3;
+        let startY = (y % 3) * 3;
         for (let blockY = startY; blockY < startY+3; blockY++){
             for (let blockX = startX; blockX < startX+3; blockX++){
                 block.push(sudoku[blockY][blockX]);
             }
         }
-
         if (hasDuplicates(row) || hasDuplicates(col) || hasDuplicates(block)) return false;
     }
     return true;
@@ -62,39 +181,59 @@ function createSudoku(){
 				['', '', '', '', '', '', '', '', ''], 
 				['', '', '', '', '', '', '', '', '']];
     let numSetted = 0;
+    let solvable = false;
+    while (!solvable){
+        while (numSetted < 20) {
+            let randX = Math.floor(Math.random() * 9);
+            let randY = Math.floor(Math.random() * 9);
+            let randNum = Math.floor(Math.random() * 9)+1;
+            if (sudoku[randY][randX]==''){
+                let row = sudoku[randY];
+                let col = [];
+                for (let index = 0; index < 9; index++) {
+                    col.push(sudoku[index][randX]);
+                }
 
-    while (numSetted < 20) {
-        let randX = Math.floor(Math.random() * 8)+1;
-        let randY = Math.floor(Math.random() * 8)+1;
-        let randNum = Math.floor(Math.random() * 8)+1;
+                let block = [];
+                let bX = Math.floor(randX / 3);
+                let bY = Math.floor(randY / 3);
+                let startX = bX * 3;
+                let startY = bY * 3;
 
-        let row = sudoku[randY];
-        let col = [];
-        for (let index = 0; index < 9; index++) {
-            col.push(sudoku[index][randX]);
-        }
+                for (let y = startY; y < startY+3; y++) {
+                    for (let x = startX; x < startX+3; x++){
+                        block.push(sudoku[y][x]);
+                    }
+                }
 
-        let block = [];
-        let bX = Math.floor(randX / 3);
-        let bY = Math.floor(randY / 3);
-        let startX = bX * 3;
-        let startY = bY * 3;
-
-        for (let y = startY; y < startY+3; y++) {
-            for (let x = startX; x < startX+3; x++){
-                block.push(sudoku[y][x]);
+                if (!inArray(row, randNum) && !inArray(col, randNum) && !inArray(block, randNum)){
+                    sudoku[randY][randX] = randNum;
+                    numSetted++;
+                }
             }
         }
-
-        if (!inArray(row, randNum) && !inArray(col, randNum) && !inArray(block, randNum)){
-            sudoku[randY][randX] = randNum;
-            numSetted++;
+        let tempS = sudoku;
+        if (isSolvable(tempS,0,0)){
+            solvable = true;
+        } else {
+            console.log('intento fallido')
+            numSetted = 0;
+            sudoku=[['', '', '', '', '', '', '', '', ''], 
+                    ['', '', '', '', '', '', '', '', ''], 
+                    ['', '', '', '', '', '', '', '', ''], 
+                    ['', '', '', '', '', '', '', '', ''], 
+                    ['', '', '', '', '', '', '', '', ''], 
+                    ['', '', '', '', '', '', '', '', ''], 
+                    ['', '', '', '', '', '', '', '', ''], 
+                    ['', '', '', '', '', '', '', '', ''], 
+                    ['', '', '', '', '', '', '', '', '']];
         }
     }
     return sudoku;
 }
 
 function printSudoku(sudoku){
+    document.getElementsByTagName('p')[0].innerHTML = '';
     elements = document.getElementsByTagName("input");
     for (let index = 0; index < elements.length; index++) {
         const element = elements[index];
